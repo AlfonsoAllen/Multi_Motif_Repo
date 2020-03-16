818839e3a54d6db01fda9129d5c3cb6b336aa9f3
Alfonso Allen-Perkins <alfonso.allen@hotmail.com>
2020-03-16 13:52:35 +0100

    Comparison between real and synthetic data
    Files to compare Caracole's real data for 2019 with null model samples

--- Hetero_Motifs_COMPARISSON.R
+++ Hetero_Motifs_COMPARISSON.R
@@ -7,50 +7,50 @@ library(tidyverse)
 
 # Loading data for Caracoles and random samples
 
-Homo_data_subplot <- read_csv("Total_Homo_data_subplot.csv")
+hetero_data_subplot <- read_csv("Total_hetero_data_subplot.csv")
 
-Prediction_homo_subplot <- Homo_data_subplot %>%
+Prediction_hetero_subplot <- hetero_data_subplot %>%
   select(Plot,Subplot,G_F,Plant_Label,motifs_Caracoles) %>%
   mutate(Mean_PI=NA,lwr_PI=NA,upr_PI=NA,Caracoles_In=NA)
 
 
-for (i in 1:nrow(Homo_data_subplot)){
+for (i in 1:nrow(hetero_data_subplot)){
 
-  sample_i <- gather(Homo_data_subplot[i,6:ncol(data_table)], key = "sample", value = "homo")
+  sample_i <- gather(hetero_data_subplot[i,6:ncol(hetero_data_subplot)], key = "sample", value = "hetero")
 
-  PI<-predict(lm(sample_i$homo~ 1), interval="predict")
+  PI<-predict(lm(sample_i$hetero~ 1), interval="predict")
   #PI[1,]
   
-  Prediction_homo_subplot$Mean_PI[i] <- PI[1,1]
-  Prediction_homo_subplot$lwr_PI[i] <- PI[1,2]
-  Prediction_homo_subplot$upr_PI[i] <- PI[1,3]
-  Prediction_homo_subplot$Caracoles_In[i] <- (PI[1,2] <= Homo_data_subplot[i,5])&
-    (Homo_data_subplot[i,5] <= PI[1,3])
+  Prediction_hetero_subplot$Mean_PI[i] <- PI[1,1]
+  Prediction_hetero_subplot$lwr_PI[i] <- PI[1,2]
+  Prediction_hetero_subplot$upr_PI[i] <- PI[1,3]
+  Prediction_hetero_subplot$Caracoles_In[i] <- (PI[1,2] <= hetero_data_subplot[i,5])&
+    (hetero_data_subplot[i,5] <= PI[1,3])
   
 
 }
 
-Prediction_homo_subplot %>% filter(Caracoles_In==FALSE,motifs_Caracoles==0)
-Prediction_homo_subplot %>% filter(Caracoles_In==FALSE,motifs_Caracoles>0)
+Prediction_hetero_subplot %>% filter(Caracoles_In==FALSE,motifs_Caracoles==0)
+Prediction_hetero_subplot %>% filter(Caracoles_In==FALSE,motifs_Caracoles>0)
 
 ###############################################3
-# It seems that homotifs per (plot,subplot) do not follow a normal distribution
+# It seems that heterotifs per (plot,subplot) do not follow a normal distribution
 ################################################
 i=13
-sample_i <- gather(Homo_data_subplot[i,6:ncol(data_table)], key = "sample", value = "homo")
+sample_i <- gather(hetero_data_subplot[i,6:ncol(data_table)], key = "sample", value = "hetero")
 
-PI<-predict(lm(sample_i$homo~ 1), interval="predict")
-x.test <- shapiro.test(sample_i$homo)
+PI<-predict(lm(sample_i$hetero~ 1), interval="predict")
+x.test <- shapiro.test(sample_i$hetero)
 print(x.test)
 
 plotn <- function(x,main="Histograma de frecuencias \ny distribución normal",
                   xlab="X",ylab="Densidad") {
-  min <- min(sample_i$homo)
-  max <- max(sample_i$homo)
-  media <- mean(sample_i$homo)
-  dt <- sd(sample_i$homo)
-  hist(sample_i$homo,freq=F,main=main,xlab=xlab,ylab=ylab)
+  min <- min(sample_i$hetero)
+  max <- max(sample_i$hetero)
+  media <- mean(sample_i$hetero)
+  dt <- sd(sample_i$hetero)
+  hist(sample_i$hetero,freq=F,main=main,xlab=xlab,ylab=ylab)
   curve(dnorm(x,media,dt), min, max,add = T,col="blue")
 }
 
-plotn(sample_i$homo,main="Distribución normal")#Grafico de x
+plotn(sample_i$hetero,main="Distribución normal")#Grafico de x
