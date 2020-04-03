1c2c45df1cd78bb7fd909da520e1a4f78e21177a
AlfAllenP <57431873+AlfAllenP@users.noreply.github.com>
2020-04-03 12:42:34 +0200

    Update CORRELATIONS_motif_fitness_WEEK.R

--- R_Scripts/CORRELATIONS_motif_fitness_WEEK.R
+++ R_Scripts/CORRELATIONS_motif_fitness_WEEK.R
@@ -9,6 +9,11 @@ fitness_data2 <- read_csv("Raw_data/Metadata_Pollinators_Abundances_Seeds_2019.c
 
 fitness2 <- fitness_data2 %>% filter(Year==2019,!Subplot == "OUT" & !is.na(G_F))
 
+#Remove HOMA and Lysimachia_arvensis
+
+fitness2 <- fitness2 %>% filter(!Plant_Simple%in%c("HOMA","Lysimachia_arvensis"))
+
+
 fitness2 <- fitness2 %>% select(-Order,-Family,-Superfamily,-ID2) %>%
   mutate(date_raw=as.Date(paste(Day,Month,Year,sep="/"), "%d/%m/%Y"),
          Week=as.numeric(format(date_raw, "%V")))
