c0b683d8a320fcc7cc043535ba0aa45f4aa73ef2
AlfAllenP <57431873+AlfAllenP@users.noreply.github.com>
2020-04-15 14:42:50 +0200

    Update Multiplex_PLANT_Community_Species_Fully_Connected.R

--- R_Scripts/Multiplex_PLANT_Community_Species_Fully_Connected.R
+++ R_Scripts/Multiplex_PLANT_Community_Species_Fully_Connected.R
@@ -72,6 +72,37 @@ Plot_edgelist_complete <- tibble(layer_from=plot_edge_list$species,
                                         node_to=plot_edge_list$to,
                                         weight=plot_edge_list$weight)
 
+##########
+plant_strength <- Plot_edgelist_complete %>% group_by(layer_from,node_from) %>% 
+  count(wt = weight) %>% rename(strength = n)
+
+pollinator_strength <- Plot_edgelist_complete %>% group_by(layer_from,node_to) %>% 
+  count(wt = weight) %>% rename(strength = n)
+##########
+
+Plot_edgelist_complete %>% filter(node_from == "B5 LEMA") # total strength should be 11 (check!)
+
+#Create the scaled directed list (previous list was meant to be undirected)
+
+#From plant to pollinator
+
+S_Links_Plant_Poll <- Plot_edgelist_complete %>% left_join(plant_strength,
+                                                           by=c("layer_from","node_from")) %>%
+  mutate(weight=weight/strength) %>% select(-strength)
+
+S_Links_Poll_Plant <- Plot_edgelist_complete %>% left_join(pollinator_strength,
+                                                           by=c("layer_from","node_to")) %>%
+  
+  mutate(weight=weight/strength) %>% select(-strength) %>%
+  rename(node_from=node_to,node_to=node_from)
+
+
+S_edge_list <- bind_rows(S_Links_Plant_Poll,S_Links_Poll_Plant)
+
+###############
+# To create the inter-links we rely on the previous Plot_edgelist_complete
+# Here we can extract information on interlayer connections
+
 for (i in 1:length(pollinators)){
   
   polinator_edges <- Plot_edgelist_complete %>% filter(node_to==pollinators[i])
@@ -80,25 +111,34 @@ for (i in 1:length(pollinators)){
   if (length(polinator_layers)>1){
     combination_layers <- t(combn(polinator_layers, 2))
     for (j in 1:nrow(combination_layers)){
-      interlink_i<- tibble(layer_from=combination_layers[j,1],
-                           node_from=pollinators[i],
-                           layer_to=combination_layers[j,2],
-                           node_to=pollinators[i],
-                           weight=1)
+      #For undirected networks
+      # interlink_i<- tibble(layer_from=combination_layers[j,1],
+      #                      node_from=pollinators[i],
+      #                      layer_to=combination_layers[j,2],
+      #                      node_to=pollinators[i],
+      #                      weight=1)
       
-      Plot_edgelist_complete <- bind_rows(Plot_edgelist_complete,interlink_i)
+      #For directed networks
+      interlink_i<- tibble(layer_from=c(combination_layers[j,1],combination_layers[j,2]),
+                           node_from=c(pollinators[i],pollinators[i]),
+                           layer_to=c(combination_layers[j,2],combination_layers[j,1]),
+                           node_to=c(pollinators[i],pollinators[i]),
+                           weight=c(1,1))
+      #For undirected
+      #Plot_edgelist_complete <- bind_rows(Plot_edgelist_complete,interlink_i)
+      
+      #For directed
+      S_edge_list <- bind_rows(S_edge_list,interlink_i)
     }
   }
 }
 
-Plot_edgelist_complete %>% group_by(layer_to,node_to) %>% count()
 
-Plot_edgelist_complete %>% filter(layer_to=="RAPE")
 
 
 # Replace the node names with node_ids
-Plot_edgelist_complete_ids <- 
-  Plot_edgelist_complete %>% 
+S_edge_list_ID <- 
+  S_edge_list %>% 
   left_join(physical_nodes, by=c('node_from' = 'species')) %>%  # Join for pollinators
   left_join(physical_nodes, by=c('node_to' = 'species')) %>%  # Join for plants
   select(-node_from, -node_to) %>% 
@@ -107,6 +147,18 @@ Plot_edgelist_complete_ids <-
   left_join(layer_metadata, by=c('layer_to' = 'layer_name')) %>%  # Join for plants
   select(-layer_from, -layer_to) %>% 
   select(layer_from=layer_id.x, node_from, layer_to=layer_id.y, node_to, weight)
+
+# # Replace the node names with node_ids
+# Plot_edgelist_complete_ids <- 
+#   Plot_edgelist_complete %>% 
+#   left_join(physical_nodes, by=c('node_from' = 'species')) %>%  # Join for pollinators
+#   left_join(physical_nodes, by=c('node_to' = 'species')) %>%  # Join for plants
+#   select(-node_from, -node_to) %>% 
+#   select(layer_from, node_from=node_id.x, layer_to, node_to=node_id.y, weight) %>% 
+#   left_join(layer_metadata, by=c('layer_from' = 'layer_name')) %>%  # Join for plants
+#   left_join(layer_metadata, by=c('layer_to' = 'layer_name')) %>%  # Join for plants
+#   select(-layer_from, -layer_to) %>% 
+#   select(layer_from=layer_id.x, node_from, layer_to=layer_id.y, node_to, weight)
 
 
 
@@ -119,11 +171,13 @@ setwd(folder_info)
 check_infomap() # Make sure file can be run correctly. Should return TRUE
 
 # Prepare data
-Plot_multilayer <- create_multilayer_object(extended = Plot_edgelist_complete_ids, nodes = physical_nodes, intra_output_extended = T, inter_output_extended = T)
+#Plot_multilayer <- create_multilayer_object(extended = Plot_edgelist_complete_ids, nodes = physical_nodes, intra_output_extended = T, inter_output_extended = T)
+Plot_multilayer <- create_multilayer_object(extended = S_edge_list_ID, nodes = physical_nodes, intra_output_extended = T, inter_output_extended = T)
 
 
 # Run Infomap
-modules_relax_rate <- run_infomap_multilayer(Plot_multilayer, relax = F, silent = T, flow_model = 'undirected', trials = 250, seed = 497294, temporal_network = F)
+#modules_relax_rate <- run_infomap_multilayer(Plot_multilayer, relax = F, silent = T, flow_model = 'undirected', trials = 250, seed = 497294, temporal_network = F)
+modules_relax_rate <- run_infomap_multilayer(Plot_multilayer, relax = F, silent = T, flow_model = 'directed', trials = 1000, seed = 497294, temporal_network = F)
 
 # Extract information
 plot_modules <- modules_relax_rate$modules %>% left_join(layer_metadata,by="layer_id")
@@ -189,7 +243,7 @@ ggplot(plot_modules_pollinators,
 
 plot_modules_pollinators <- plot_modules_pollinators %>%arrange(module)
 
-write_csv(plot_modules_pollinators,"plot_8_modules_pollinators_full_connected.csv")
+write_csv(plot_modules_pollinators,"plot_8_modules_pollinators_full_connected_SCALED.csv")
 
 # Flexibility of species 
 plot_modules %>%
@@ -200,4 +254,189 @@ plot_modules %>%
 
 
 setwd(dir_ini)
+
+  
+######
+library(circlize)
+# https://jokergoo.github.io/circlize_book/book/the-chorddiagram-function.html
+
+#colormaps
+library(viridis)           # Load
+library(RColorBrewer)
+
+
+test_mod0 <- plot_modules %>% filter(type!="plant")%>%
+  group_by(layer_name,module) %>% count() %>%
+  select(layer_name,module,n) %>% rename(from =layer_name,to=module,value=n)
+
+
+c <- length(unique(test_mod0$from))
+
+
+
+grid.col0 <- setNames(c(magma(c),
+                        rep("grey",length(unique(test_mod0$to)))),
+                      union(test_mod0$from, test_mod0$to))
+
+par(cex = .7, mar = c(0, 0, 0, 0))
+chordDiagram(test_mod0, preAllocateTracks = 1,
+             order=union(test_mod0$from, rev(test_mod0$to)),
+             big.gap = 10,grid.col=grid.col0,
+             symmetric=FALSE,
+             link.arr.type = "big.arrow",
+             #transparency of colours
+             transparency = 0.2,
+             scale = TRUE
+)
+
+
+circos.clear()
+
+###########
+
+
+
+test_mod1 <- plot_modules %>% filter(type!="plant")%>%
+  group_by(species,module) %>% count() %>%
+  select(species,module,n) %>% rename(from =species,to=module,value=n)
+
+a <- 9
+b <- 9
+c <- length(unique(test_mod1$from))- a-b
+
+
+
+grid.col1 <- setNames(c(brewer.pal(n = a, name = "Set1"),
+                        brewer.pal(n = b, name = "Pastel1"),
+                        magma(c),
+                        rep("grey",length(unique(test_mod1$to)))),
+                      union(test_mod1$from, test_mod1$to))
+
+par(cex = .7, mar = c(0, 0, 0, 0))
+chordDiagram(test_mod1,annotationTrack = "grid", preAllocateTracks = 1,
+             order=union(test_mod1$from, rev(test_mod1$to)),
+             big.gap = 10,grid.col=grid.col1,
+             symmetric=FALSE,
+             link.arr.type = "big.arrow",
+             #transparency of colours
+             transparency = 0.2,
+             scale = TRUE
+)
+circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+  xlim = get.cell.meta.data("xlim")
+  ylim = get.cell.meta.data("ylim")
+  sector.name = get.cell.meta.data("sector.index")
+  circos.text(mean(xlim), ylim[1] + .1, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
+  circos.axis(h = "top", labels.cex = 0.5, major.tick.percentage = 0.2, sector.index = sector.name, track.index = 2)
+}, bg.border = NA)
+
+circos.clear()
+
+###########
+
+
+test_mod2 <- plot_modules %>% filter(type!="plant")%>%
+  group_by(species,layer_name) %>% count() %>%
+  select(species,layer_name,n) %>% rename(from =species,to=layer_name,value=n)
+
+a <- 9
+b <- 9
+c <- length(unique(test_mod2$from))- a-b
+
+
+
+grid.col2 <- setNames(c(brewer.pal(n = a, name = "Set1"),
+                        brewer.pal(n = b, name = "Pastel1"),
+                        magma(c),
+                        rep("grey",length(unique(test_mod2$to)))),
+                      union(test_mod2$from, test_mod2$to))
+
+par(cex = .7, mar = c(0, 0, 0, 0))
+chordDiagram(test_mod2,annotationTrack = "grid", preAllocateTracks = 1,
+             order=union(test_mod2$from, rev(test_mod2$to)),
+             big.gap = 10,grid.col=grid.col2,
+             symmetric=FALSE,
+             link.arr.type = "big.arrow",
+             #transparency of colours
+             transparency = 0.2,
+             scale = TRUE
+)
+circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+  xlim = get.cell.meta.data("xlim")
+  ylim = get.cell.meta.data("ylim")
+  sector.name = get.cell.meta.data("sector.index")
+  circos.text(mean(xlim), ylim[1] + .1, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
+  circos.axis(h = "top", labels.cex = 0.5, major.tick.percentage = 0.2, sector.index = sector.name, track.index = 2)
+}, bg.border = NA)
+
+circos.clear()
+
+
+###################
+#https://www.r-graph-gallery.com/309-intro-to-hierarchical-edge-bundling.html
+
+layout(matrix(1:2, 1, 2)) 
+par(cex = .7, mar = c(0, 0, 0, 0))
+chordDiagram(test_mod1,annotationTrack = "grid", preAllocateTracks = 1,
+             order=union(test_mod1$from, rev(test_mod1$to)),
+             big.gap = 10,grid.col=grid.col1,
+             symmetric=FALSE,
+             link.arr.type = "big.arrow",
+             #transparency of colours
+             transparency = 0.2,
+             scale = TRUE
+)
+circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+  xlim = get.cell.meta.data("xlim")
+  ylim = get.cell.meta.data("ylim")
+  sector.name = get.cell.meta.data("sector.index")
+  circos.text(mean(xlim), ylim[1] + .1, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
+  circos.axis(h = "top", labels.cex = 0.5, major.tick.percentage = 0.2, sector.index = sector.name, track.index = 2)
+}, bg.border = NA)
+#title("Modules (Plot 8)", cex = 1.0)
+circos.clear()
+
+#########
+
+fitness_data2 <- read_csv("Raw_Data/Metadata_Pollinators_Abundances_Seeds_2019_ID.csv")
+
+# Filtering & relabeling
+fitness2 <- fitness_data2 %>% filter(Year==2019,!Subplot == "OUT" & !is.na(ID))
+test3 <- fitness2 %>% select(G_F,ID) %>% rename(from=ID)
+test_mod3 <- test_mod1 %>% left_join(unique(test3),by="from")  
+test_mod3 <- test_mod3 %>% ungroup() %>% select(-to) %>% rename(to=G_F) %>%select(from,to,value)%>%
+  mutate(to=paste(to,"_GF",sep=""))
+test_mod3$value <- 1
+
+a <- 9
+b <- 9
+c <- length(unique(test_mod3$from))- a-b
+
+
+
+grid.col3 <- setNames(c(brewer.pal(n = a, name = "Set1"),
+                        brewer.pal(n = b, name = "Pastel1"),
+                        magma(c),
+                        rep("grey",length(unique(test_mod3$to)))),
+                      union(test_mod3$from, test_mod3$to))
+
+par(cex = .7, mar = c(0, 0, 0, 0))
+chordDiagram(test_mod3,annotationTrack = "grid", preAllocateTracks = 1,
+             order=union(test_mod3$from, rev(test_mod3$to)),
+             big.gap = 10,grid.col=grid.col3,
+             symmetric=FALSE,
+             link.arr.type = "big.arrow",
+             #transparency of colours
+             transparency = 0.2,
+             scale = TRUE
+)
+circos.trackPlotRegion(track.index = 1, panel.fun = function(x, y) {
+  xlim = get.cell.meta.data("xlim")
+  ylim = get.cell.meta.data("ylim")
+  sector.name = get.cell.meta.data("sector.index")
+  circos.text(mean(xlim), ylim[1] + .1, sector.name, facing = "clockwise", niceFacing = TRUE, adj = c(0, 0.5))
+  circos.axis(h = "top", labels.cex = 0.5, major.tick.percentage = 0.2, sector.index = sector.name, track.index = 2)
+}, bg.border = NA)
+#title("Functional Groups (Plot 8)", cex = 1.0)
+circos.clear()
 
